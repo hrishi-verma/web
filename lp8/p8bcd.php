cf1780d98884ae37a7deb056ccc6f891a2a81230
hrishikesh verma <bjinsi@yahoo.com>
2019-12-02 18:03:37 +0530

    Update p8bcd.php

--- lp8/p8bcd.php
+++ lp8/p8bcd.php
@@ -7,44 +7,44 @@ $p=count($b);
 $q=count($b[2]);
 echo "the first matrix :"."<br/>";
 for ($row = 0; $row < $m; $row++) {
-for ($col = 0; $col < $n; $col++)
-echo " ".$a[$row][$col];
-echo "<br/>";
+    for ($col = 0; $col < $n; $col++)
+        echo " ".$a[$row][$col];
+        echo "<br/>";
 }
 echo "the second matrix :"."<br/>";
 for ($row = 0; $row < $p; $row++) {
-for ($col = 0; $col < $q; $col++)
-echo " ".$b[$row][$col];
-echo "<br/>";
+    for ($col = 0; $col < $q; $col++)
+        echo " ".$b[$row][$col];
+        echo "<br/>";
 }
 echo "the transpose for the first matrix is:"."<br/>";
 for ($row = 0; $row < $m; $row++) {
-for ($col = 0; $col < $n; $col++)
-echo " ".$a[$col][$row];
-echo "<br/>";
+    for ($col = 0; $col < $n; $col++)
+        echo " ".$a[$col][$row];
+        echo "<br/>";
 }
 if(($m===$p) and ($n===$q)) {
-echo "the addition of matrices is:"."<br/>";
-for ($row = 0; $row < 3; $row++) {
-for ($col = 0; $col < 3; $col++)
-echo " ".$a[$row][$col]+$b[$row][$col]." ";
-echo "<br/>";
-} }
+    echo "the addition of matrices is:"."<br/>";
+    for ($row = 0; $row < 3; $row++) {
+          for ($col = 0; $col < 3; $col++)
+              echo " ".$a[$row][$col]+$b[$row][$col]." ";
+              echo "<br/>";
+    } 
+}
 if($n===$p){
-echo " The multiplication of matrices: <br/>";
-$result=array();
-for ($i=0; $i < $m; $i++) {
-for($j=0; $j < $q; $j++){
-$result[$i][$j] = 0;
-for($k=0; $k < $n; $k++)
-$result[$i][$j] += $a[$i][$k] * $b[$k][$j];
-} 
-}
-for ($row = 0; $row < $m; $row++)
-{
-for ($col = 0; $col < $q; $col++)
-echo " ".$result[$row][$col];
-echo "<br/>";
-}
+    echo " The multiplication of matrices: <br/>";
+    $result=array();
+    for ($i=0; $i < $m; $i++) {
+        for($j=0; $j < $q; $j++){
+            $result[$i][$j] = 0;
+            for($k=0; $k < $n; $k++)
+                $result[$i][$j] += $a[$i][$k] * $b[$k][$j];
+        } 
+    }
+    for ($row = 0; $row < $m; $row++){
+        for ($col = 0; $col < $q; $col++)
+            echo " ".$result[$row][$col];
+            echo "<br/>";
+    }
 }
 ?>
