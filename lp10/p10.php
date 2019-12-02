739b69cbdd7300fff6e574d2603068f956e0db45
hrishikesh verma <bjinsi@yahoo.com>
2019-12-02 17:56:15 +0530

    Update p10.php

--- lp10/p10.php
+++ lp10/p10.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html> 
 <body> 
-<style> 
-table, td, th 
-{ 
-border: 1px solid black; 
-width: 33%; 
-text-align: center; 
-border-collapse:collapse; 
-background-color:lightblue; } 
-table { margin: auto; } 
-</style> 
+  <style> 
+    table, td, th 
+    { 
+    border: 1px solid black; 
+    width: 33%; 
+    text-align: center; 
+    border-collapse:collapse; 
+    background-color:lightblue; } 
+    table { margin: auto; } 
+  </style> 
 <?php
 $servername = "localhost";
 $username = "root";
@@ -21,7 +21,7 @@ $a=[];
 // Opens a new connection to the MySQL server
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if ($conn->connect_error)
-die("Connection failed: " . $conn->connect_error);
+   die("Connection failed: " . $conn->connect_error);
 $sql = "SELECT * FROM student";
 // performs a query against the database
 $result = $conn->query($sql);
@@ -32,13 +32,14 @@ echo "<tr>";
 echo "<th>USN</th><th>NAME</th><th>Address</th></tr>";
 if ($result->num_rows> 0)
 {
-while($row = $result->fetch_assoc()) {
-echo "<tr>";
-echo "<td>". $row["usn"]."</td>";
-echo "<td>". $row["name"]."</td>";
-echo "<td>". $row["address"]."</td></tr>";
-array_push($a,$row["name"]);
-} }
+  while($row = $result->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>". $row["usn"]."</td>";
+    echo "<td>". $row["name"]."</td>";
+    echo "<td>". $row["address"]."</td></tr>";
+    array_push($a,$row["name"]);
+  } 
+}
 else
 echo "Table is Empty";
 echo "</table>";
@@ -46,15 +47,15 @@ $n=count($a);
 $b=$a;
 for ( $i = 0 ; $i< ($n - 1) ; $i++ )
 {
-$pos= $i;
-for ( $j = $i + 1 ; $j < $n ; $j++ ) {
-if ( $a[$pos] > $a[$j] )
-$pos= $j;
+  $pos= $i;
+  for ( $j = $i + 1 ; $j < $n ; $j++ ) {
+  if ( $a[$pos] > $a[$j] )
+  $pos= $j;
 }
 if ( $pos!= $i ) {
-$temp=$a[$i];
-$a[$i] = $a[$pos];
-$a[$pos] = $temp;
+  $temp=$a[$i];
+  $a[$i] = $a[$pos];
+  $a[$pos] = $temp;
 }
 }
 $c=[];
@@ -62,14 +63,14 @@ $d=[];
 $result = $conn->query($sql);
 if ($result->num_rows> 0)// output data of each row
 {
-while($row = $result->fetch_assoc()) {
-for($i=0;$i<$n;$i++) {
-if($row["name"]== $a[$i]) {
-$c[$i]=$row["usn"];
-$d[$i]=$row["address"];
-}
-}
-}
+  while($row = $result->fetch_assoc()) {
+    for($i=0;$i<$n;$i++) {
+      if($row["name"]== $a[$i]) {
+        $c[$i]=$row["usn"];
+        $d[$i]=$row["address"];
+      }
+    }
+  }
 }
 echo "<br>";
 echo "<center> AFTER SORTING <center>";
@@ -77,10 +78,10 @@ echo "<table border='2'>";
 echo "<tr>";
 echo "<th>USN</th><th>NAME</th><th>Address</th></tr>";
 for($i=0;$i<$n;$i++) {
-echo "<tr>";
-echo "<td>". $c[$i]."</td>";
-echo "<td>". $a[$i]."</td>";
-echo "<td>". $d[$i]."</td></tr>";
+  echo "<tr>";
+  echo "<td>". $c[$i]."</td>";
+  echo "<td>". $a[$i]."</td>";
+  echo "<td>". $d[$i]."</td></tr>";
 }
 echo "</table>";
 $conn->close();
