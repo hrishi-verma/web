47b65ef7fbfc1a43b98755afcbaba6be8f137117
hrishikesh verma <bjinsi@yahoo.com>
2019-12-02 18:04:58 +0530

    Update p7.php

--- lp7/p7.php
+++ lp7/p7.php
@@ -3,16 +3,18 @@
 <head>
 <meta http-equiv="refresh" content="1"/>
 <style>
-p {
-color:white;
-font-size:90px;
-position: absolute;
-top: 50%;
-left: 50%;
-transform: translate(-50%, -50%);
-}
-body{background-color:black;}
+    p {
+    color:white;
+    font-size:90px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    }
+    body{background-color:black;}
 </style>
-<p> <?php echo date(" h: i : s A");?> </p>
+<p> 
+  <?php echo date(" h: i : s A");?>
+</p>
 </head>
 </html>
